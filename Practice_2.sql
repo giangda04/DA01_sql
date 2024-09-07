92c38ba0e50816212088c5bbd6f8dd26d00e1a1f
giangda04 <doanhuonggiang2204@gmail.com>
2024-09-08 01:58:53 +0700

    Update Practice_2.sql

--- Practice_2.sql
+++ Practice_2.sql
@@ -17,4 +17,53 @@ SELECT candidate_id FROM candidates
 where skill in ('Python','Tableau','PostgreSQL')
 group by candidate_id
 having count(skill) = 3 
-ORDER BY candidate_id 
+
+--ex6: write a query to find the number of days between each user’s first post of the year and last post of the year in the year 2021
+SELECT user_id, 
+date(max(post_date))-date(min(post_date)) as days_between
+FROM posts
+where EXTRACT(YEAR FROM post_date) = 2021
+group by user_id
+having count(post_id) >= 2
+
+--ex7: Write a query that outputs the name of each credit card and the difference in the number of issued cards between the month with the highest issuance cards and the lowest issuance.
+SELECT card_name,
+max(issued_amount) - min(issued_amount) as difference
+FROM monthly_cards_issued
+group by card_name
+order by difference desc
+
+--ex8: 
+SELECT manufacturer,
+COUNT(product_id) as drug_count,
+abs(sum(cogs) - sum(total_sales)) as total_losses
+FROM pharmacy_sales
+where total_sales < cogs
+GROUP BY manufacturer
+order by  total_losses DESC
+
+---cogs là cost of goods sold
+
+--ex9:
+select * from cinema
+where ID%2 =1 and not (description like "%boring%")
+order by rating desc
+
+---ex10: 
+select teacher_id,
+count(distinct(subject_id)) as cnt
+from teacher
+group by teacher_id
+
+--ex11: Write a solution that will, for each user, return the number of follower
+select  user_id,
+count(follower_id) as followers_count
+from Followers
+group by user_id
+order by user_id;
+
+--ex12: Write a solution to find all the classes that have at least five students.
+select class
+from Courses
+group by class
+having  count(student) >= 5
