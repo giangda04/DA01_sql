d170ddaf2afe93ce4d7ebd8efef9884edabf867a
giangda04 <doanhuonggiang2204@gmail.com>
2024-09-09 19:11:42 +0700

    Update Practice_2.sql

--- Practice_2.sql
+++ Practice_2.sql
@@ -5,7 +5,13 @@ where ID%2 = 0
 ---ex2: Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 select count(city) - count(distinct city) from station
 
---ex3: để lại
+/*ex3: Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, 
+but did not realize her keyboard's  key was broken until after completing the calculation.
+She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary. */
+select ceiling(avg(salary)-(avg(replace(salary,'0',''))))
+from employees
+
+Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
 
 --ex4:
 SELECT CAST(sum(item_count*order_occurrences)/sum(order_occurrences) as decimal(10,1)) 
