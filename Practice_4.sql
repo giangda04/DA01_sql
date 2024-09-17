99cfd5a1f526cedc906c853571be8f86859e48c8
giangda04 <doanhuonggiang2204@gmail.com>
2024-09-18 01:55:32 +0700

    Create Practice_4.sql

---
+++ Practice_4.sql
@@ -0,0 +1,22 @@
+/* ex1: Write a query that calculates the total viewership for laptops and mobile devices where mobile is defined as the sum of tablet and phone viewership.
+Output the total viewership for laptops as laptop_reviews and the total viewership for mobile devices as mobile_views. */
+SELECT 
+count(CASE
+    when device_type ='laptop' then 'laptop_reviews'
+    else null ---hàm count() không đếm giá trị null
+      END) as laptop_reviews,
+count(CASE
+    when device_type in ('tablet', 'phone') then 'mobile_views'
+    else null
+      END) as mobile_views
+from viewership 
+
+--ex2: Report for every three line segments whether they can form a triangle.
+/* Điều kiện để ba đoạn thẳng có thể tạo thành một tam giác là tổng độ dài của hai đoạn bất kỳ phải lớn hơn độ dài của đoạn còn lại.
+  Phải thỏa mãn cả 3 điều kiện */
+select x,y,z,
+case
+    when (x+y>z) and (x+z>y) and (z+y> x)  then 'Yes'
+    else 'No'
+end as triangle
+from Triangle
